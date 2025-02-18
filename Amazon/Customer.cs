55b4fa697dfc5acd2679a33fbb15519b6a80e07b
DaudCh <daudch42@gmail.com>
2025-02-18 13:03:23 +0500

    Add project files.

---
+++ Amazon/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public void Promote()
+        {
+            var rating = CalculateRating(excludeorders: true);
+            if (rating == 0)
+            {
+                Console.WriteLine("Level 1");
+            }
+            else
+            {
+                Console.WriteLine("Level 2");
+            }
+        }
+
+        public int CalculateRating(bool excludeorders)
+        {
+            return 0; // Placeholder logic
+        }
+    }
+
+}
---
+++ FirstProject/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstProject
+{
+
+    public interface activity
+    {
+         void execute();
+    }
+    public class UploadVideo : activity
+    {
+        public void execute() {
+            Console.WriteLine("video Uploaded");
+
+        }
+    }
+    public class WorkFlow
+    {
+        private readonly List<activity> _activities;
+        public WorkFlow()
+        {
+            _activities = new List<activity>();
+            
+        }
+        public void Addactivity(activity activity)
+        {
+            _activities.Add(activity);
+        }
+        public IEnumerable<activity> Getactivities()
+        {
+            return _activities;
+            
+        }
+         
+    }
+
+    public class WorkFlowEngine
+    {
+        public void Run(WorkFlow workFlow)
+        {
+            foreach(var act in workFlow.Getactivities())
+            {
+                act.execute();
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var workflow = new WorkFlow();
+            workflow.Addactivity( new UploadVideo());
+
+            var workFlowEngine = new WorkFlowEngine();
+            workFlowEngine.Run(workflow);
+
+            Console.WriteLine("Executed");
+
+
+        }
+
+    }
+}
+    
+
